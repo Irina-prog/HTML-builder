59c6cec828e2f1c5fb07e96905b13b496270c8b1
“Irina <ira.bel4ikova@gmail.com>
2023-05-03 22:50:10 +0400

    feat: task 2

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 fs
-  .createReadStream(path.join(__dirname, 'text.txt'))
-  .pipe(process.stdout);
+  .createReadStream(path.join(__dirname, 'text.txt')) // создали файловый поток на чтения для указанного в параметре файла
+  .pipe(process.stdout); // файловый поток чтения перенаправляем
+  // на standard output - поток на запись в консоль
